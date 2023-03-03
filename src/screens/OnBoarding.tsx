fe7bc473d3612dacb73316926fb8361cc760cd14
charaf marghin <developermarghin@gmail.com>
2023-03-03 22:33:21 +0100

    add initial screens

---
+++ src/screens/OnBoarding.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const OnBoarding = (): JSX.Element => {
+  return (
+    <View>
+      <Text className="text-4xl text-red-400">OnBoarding</Text>
+    </View>
+  );
+};
+
+export default OnBoarding;
