19eff6a82915fe45102ec0617ef1d68ffbadf4c2
charaf marghin <developermarghin@gmail.com>
2023-03-05 20:03:58 +0100

    add splash screen for android

--- android/app/src/main/java/com/watchify/MainActivity.java
+++ android/app/src/main/java/com/watchify/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen; // here
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -19,7 +20,8 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this);  // here
+    super.onCreate(savedInstanceState);
   }
 
   /**
