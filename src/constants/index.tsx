aced1655154a5b8f785484bd4029a84e0685c977
charaf marghin <developermarghin@gmail.com>
2023-03-05 01:24:48 +0100

    project color theme

---
+++ src/constants/index.tsx
@@ -0,0 +1,7 @@
+export const colors = {
+  primary_bg: '#000814',
+  secondary_bg: '#1b263b',
+  primary_text: '#fff',
+  secondary_text: '#b3b3b3',
+  primary: '#ffc300',
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_bg: '#000814',
+        secondary_bg: '#1b263b',
+        primary_text: '#fff',
+        secondary_text: '#b3b3b3',
+        primary: '#ffc300',
+      },
+    },
   },
   plugins: [],
 };
