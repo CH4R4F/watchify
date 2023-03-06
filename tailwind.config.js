1c3fef2e1b20d671fbe2cd64b5eeb087741ee309
charaf marghin <developermarghin@gmail.com>
2023-03-06 16:31:13 +0100

    add tailwind fonts utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
         primary: '#ffc300',
         secondary: '#ffd60aaa',
       },
+
+      fontFamily: {
+        'text-200': ['Lato-Light', 'sans-serif'],
+        'text-400': ['Lato-Regular', 'sans-serif'],
+        'text-600': ['Lato-Bold', 'sans-serif'],
+        'text-700': ['Lato-Black', 'sans-serif'],
+        'heading-400': ['Poppins-Regular', 'sans-serif'],
+        'heading-500': ['Poppins-Medium', 'sans-serif'],
+        'heading-600': ['Poppins-Bold', 'sans-serif'],
+        'heading-700': ['Poppins-Black', 'sans-serif'],
+      },
     },
   },
   plugins: [],
