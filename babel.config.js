b4f6e04c53a8bef78208e7dfca74fad4d683b37b
charaf marghin <developermarghin@gmail.com>
2023-03-06 11:28:46 +0100

    add tmdb api key with dotenv configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env', // used to import env variables
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        blacklist: null, // DEPRECATED
+        whitelist: null, // DEPRECATED
+        safe: false,
+        allowUndefined: false,
+        verbose: false,
+      },
+    ],
+  ],
 };
