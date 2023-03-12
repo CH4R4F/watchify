884b6f9dd4683354a1f435a9db77913037dc7242
charaf marghin <developermarghin@gmail.com>
2023-03-12 01:25:43 +0100

    remove unused properties

--- src/navigatios/stack.tsx
+++ src/navigatios/stack.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnBoarding from '../screens/OnBoarding';
 import MainNavigation from './main';
-import {colors} from '../constants';
 
 const Stack = createStackNavigator();
 
